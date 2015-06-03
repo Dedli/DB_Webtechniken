14e28964f27fdd9805707a12b6e2ef0a223b3c59
daniel <ddet@hrz.tu-chemnitz.de>
2015-06-03 16:50:17 +0200

    Administrationsoberfläche

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'testuser' });
+  res.render('admin', { title: 'testuser',
+                        FS: ['Chemie','Informatik','Mathematik','Elektrotechnik/Informationstechnik','Philosophische Fakult\u00e4t','Wirtschaftswissenschaften','Human- und Sozialwissenschaften','Physik'],
+                        gremien: ['SturRa','FSR']
+  });
 });
 
 module.exports = router;
