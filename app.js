49bbfbebd0a9fe714630fe02fa32295c7e622a20
daniel <ddet@hrz.tu-chemnitz.de>
2015-06-12 13:35:24 +0200

    login angefangen

--- app.js
+++ app.js
@@ -4,12 +4,106 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var admin = require('./routes/admin');
+var login = require('./routes/login');
 var app = express();
 
+// Init Passport
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use('login', new LocalStrategy({
+      passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      // check in mongo if a user with username exists or not
+      User.findOne({ 'username' :  username },
+          function(err, user) {
+            // In case of any error, return using the done method
+            if (err)
+              return done(err);
+            // Username does not exist, log error & redirect back
+            if (!user){
+              console.log('User Not Found with username '+username);
+              return done(null, false,
+                  req.flash('message', 'User Not found.'));
+            }
+            // User exists but wrong password, log the error
+            if (!isValidPassword(user, password)){
+              console.log('Invalid Password');
+              return done(null, false,
+                  req.flash('message', 'Invalid Password'));
+            }
+            // User and password both match, return user from
+            // done method which will be treated like success
+            return done(null, user);
+          }
+      );
+    }));
+
+passport.use('signup', new LocalStrategy({
+      passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      findOrCreateUser = function(){
+        // find a user in Mongo with provided username
+        User.findOne({'username':username},function(err, user) {
+          // In case of any error return
+          if (err){
+            console.log('Error in SignUp: '+err);
+            return done(err);
+          }
+          // already exists
+          if (user) {
+            console.log('User already exists');
+            return done(null, false,
+                req.flash('message','User Already Exists'));
+          } else {
+            // if there is no user with that email
+            // create the user
+            var newUser = new User();
+            // set the user's local credentials
+            newUser.username = username;
+            newUser.password = createHash(password);
+            newUser.email = req.param('email');
+            newUser.firstName = req.param('firstName');
+            newUser.lastName = req.param('lastName');
+
+            // save the user
+            newUser.save(function(err) {
+              if (err){
+                console.log('Error in Saving user: '+err);
+                throw err;
+              }
+              console.log('User Registration succesful');
+              return done(null, newUser);
+            });
+          }
+        });
+      };
+
+      // Delay the execution of findOrCreateUser and execute
+      // the method in the next tick of the event loop
+      process.nextTick(findOrCreateUser);
+    })
+);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,6 +119,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admin', admin);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
